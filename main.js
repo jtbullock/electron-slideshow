79af6776ca2e555176c573eb0b7f56d4ffec3757
Josh Bullock <mail@joshb.me>
2016-01-10 22:23:15 -0500

    Add ability to choose directory

--- main.js
+++ main.js
@@ -10,27 +10,33 @@ const ipc = electron.ipcMain;
 
 const fs = require('fs');
 
+const dialog = require('electron').dialog;
+
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+    // and load the index.html of the app.
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 }
 
 // This method will be called when Electron has finished
@@ -38,26 +44,31 @@ function createWindow () {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on('window-all-closed', function() {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+app.on('activate', function() {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
 
-ipc.on('GetElectronVersion', function() {
-  fs.readdir('/Users/joshbullock/Dropbox/Pictures/Animals', function(err, files) {
-     console.log(files); 
-  });
+ipc.on('OpenDirectoryDialog', function(event) {
+    var directory = dialog.showOpenDialog(mainWindow, {
+        title: "Choose Directory",
+        properties: ["openDirectory"]
+    });
     
-  console.log('Get Electron Version called');
+    console.log("Directory selected: " + directory);
+    
+    if(directory != null && directory.length > 0) {
+        event.sender.send('DirectorySelected', directory[0]);
+    }
 });
